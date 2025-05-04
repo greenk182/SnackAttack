63b3473f7e121b3db2bfd2379b03ee009a0da51f
Kevin Green <kevin@connectifi.co>
2025-05-04 10:12:32 -0400

    Initial Commit

---
+++ SnackAttack/SnackAttackApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnackAttackApp.swift
+//  SnackAttack
+//
+//  Created by Kevin Green on 5/4/25.
+//
+
+import SwiftUI
+
+@main
+struct SnackAttackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SnackAttackTests/SnackAttackTests.swift
@@ -0,0 +1,17 @@
+//
+//  SnackAttackTests.swift
+//  SnackAttackTests
+//
+//  Created by Kevin Green on 5/4/25.
+//
+
+import Testing
+@testable import SnackAttack
+
+struct SnackAttackTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
